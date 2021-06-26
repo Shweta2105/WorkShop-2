f314037fcf6ccae066897daf232a918acfff91ed
Shweta2105 <sghadi21@gmail.com>
2021-06-26 08:51:25 +0530

    Welcome message passed

---
+++ main/java/wrokshop_2/hotelReservation/App.java
@@ -0,0 +1,13 @@
+package wrokshop_2.hotelReservation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ main/java/wrokshop_2/hotelReservation/HotelMain.java
@@ -0,0 +1,8 @@
+package wrokshop_2.hotelReservation;
+
+public class HotelMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Resvervation System");
+	}
+
+}
